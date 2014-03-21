cb1965c48d3f10a2d274bb893333e37c06a6334d
Joanna Rajewska <joanna.rajewska@netguru.pl>
2014-03-21 15:41:29 +0000

    mimonalizm + php

--- contact-post.php
+++ contact-post.php
@@ -1,25 +1,20 @@
 <?php
-$body = "Dzien dobry: \n\n";
-$body .= 'name: ' . strip_tags($_POST['userName']) . "\n";
-$body .= 'surname: ' . strip_tags($_POST['userSurname']) . "\n";
-$body .= 'email: ' . strip_tags($_POST['userEmail']) . "\n";
-$body .= 'phone: ' . strip_tags($_POST['userPhone']) . "\n\n";
-$body .= 'subject: ' . strip_tags($_POST['subject']) . "\n\n";
-$body .= strip_tags($_POST['message']) . "\n";
-$headers = 'From: contact@domena.com' . "\r\n" .
-   'Reply-To: domena@domena.com' . "\r\n";
-$retval = mail("domena@domena.com", strip_tags($_POST['subject']), $body, $headers);
+$body = "Nowy człowiek zainteresowany językami: \n\n";
+$body .= 'email: ' . strip_tags($_POST['email']) . "\n";
+$headers = 'From: contact@rajewska.asia' . "\r\n" .
+   'Reply-To: '. strip_tags($_POST['email']) . "\r\n";
+$retval = mail("contact@rajewska.asia", "Nowy człowiek zainteresowany językami", $body, $headers);
    if( $retval == true )
    {
       echo ("<SCRIPT LANGUAGE='JavaScript'>
-        window.alert('Message sent successfully...')
+        window.alert('Thank you!')
         window.location.href='./index.html';
       </SCRIPT>");
    }
    else
    {
       echo ("<SCRIPT LANGUAGE='JavaScript'>
-        window.alert('Message could not be sent...')
+        window.alert('Error. Try again.')
         window.location.href='./index.html';
       </SCRIPT>");
    }
